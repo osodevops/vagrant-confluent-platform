9e2b797e3d90b6f88077f00d1f893d8f9fbe6610
Andrew McCully <mccullya@gmail.com>
2022-12-09 09:22:31 +0000

    Locking in vagrant build to 7.2.2

--- provisioners/scripts/install_confluent_platform.sh
+++ provisioners/scripts/install_confluent_platform.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eux
-ansible-galaxy collection install confluent.platform
+ansible-galaxy collection install confluent.platform:7.2.2
 ansible-playbook -i /home/vagrant/provisioners/ansible/inventory/1node.yml confluent.platform.all
